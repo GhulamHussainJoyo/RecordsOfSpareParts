ce76523adb85a55b660afec2c30618feb39acf7b
Ghulam Hussian Joyo <ghulamhussain.kcsf18@iba-suk.edu.pk>
2020-07-17 21:49:12 +0500

    This is App for shop keepers who want to keep himself update about his glosseries

--- app/src/main/java/com/example/appforspareparts/DashBoard.java
+++ app/src/main/java/com/example/appforspareparts/DashBoard.java
@@ -11,10 +11,12 @@ public class DashBoard extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_dash_board);
-            ///
+            ///dfghjk
     }
 
-    private void Abbal(String n) {
+    private void Abbal(String n)
+    {
+            //chal be chilgoze
 
     }
 }
